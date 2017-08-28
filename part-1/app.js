7014e88145a92dc69fd00cbba13f8c012e838450
Jay Cribas <feverjay@gmail.com>
2017-08-28 12:49:17 -0700

    api/days routes correctly respond to url param

--- part-1/app.js
+++ part-1/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const daysApi = require('./routes/days-api')
+const concatApi = require('./routes/concat-api')
 
-app.get('/', function(req, res){
-  res.send('Hello world')
-})
+app.use('/api/days', daysApi)
+app.use('/api/array/concat', concatApi)
 
 app.listen(3000, function(){
   console.log('Listening on port 3000')
