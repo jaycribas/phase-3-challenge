6cc5992a8a442f55075c901abfdac1ea881d6020
Jay Cribas <feverjay@gmail.com>
2017-08-30 11:10:32 -0700

    Modal scaffolding with open/close functionality

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("I'm here!")
-});
+  const cartItems = []
+  const cartModal = document.getElementById('cart-button')
+  const modalBtn = document.getElementById('cart-button')
+  const modalClose = document.getElementsByClassName('close')[0]
+
+  // Open modal
+  cartModal.addEventListener('click', function(){
+    document.getElementById('modal').style.display = 'block'
+  })
+
+  // Close modal
+  modalClose.addEventListener('click', function(){
+    document.getElementById('modal').style.display = 'none'
+  })
+
+
+})
