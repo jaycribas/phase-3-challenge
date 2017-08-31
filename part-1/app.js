ff5a2d9e667fac58bb5213a1ed09315bfc8c3cae
Jay Cribas <feverjay@gmail.com>
2017-08-31 13:07:19 -0700

    Refactors usage of body-parser

--- part-1/app.js
+++ part-1/app.js
@@ -5,11 +5,10 @@ const bodyParser = require('body-parser')
 const daysRouter = require('./routes/days')
 const concatRouter = require('./routes/concat')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const bodyParserJson = bodyParser.json()
 
 app.use('/api/days', daysRouter)
-app.use('/api/array/concat', concatRouter)
+app.use('/api/array/concat', bodyParserJson, concatRouter)
 
 app.listen(3000, function(){
   console.log('Listening on port 3000')
--- part-1/routes/concat.js
+++ part-1/routes/concat.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 
 router.post('/', function(req, res){
   let { array1, array2 } = req.body
-  array1 = JSON.parse(array1)
-  array2 = JSON.parse(array2)
   if(Array.isArray(array1) && Array.isArray(array2))
     res.json({ "result": array1.concat(array2) })
   else
